459d785e1135b388f428c6ee36ce55342563a837
mikeder <mikedernet@gmail.com>
2018-10-04 20:36:15 -0400

    testing handlers

--- main.go
+++ main.go
@@ -5,18 +5,30 @@ import (
   "log"
   "net/http"
   "net/response"
+  "time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "root")
+}
+
+func sleep(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "sleep")
+  time.Sleep(5 * time.Second)
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("content-type", "application/json")
-  resp := &response.BasicResponse{
+  resp := response.BasicResponse{
     Message: "success",
     Code: http.StatusOK,
     Response: r.URL.Path}
-  fmt.Fprintln(w, response.JsonResponse(resp))
+  fmt.Fprintln(w, response.JsonResponse(&resp))
 }
 
 func main()  {
-  http.HandleFunc("/", handler) // each request calls handler
+  http.HandleFunc("/", root)
+  http.HandleFunc("/sleep", sleep)
+  http.HandleFunc("/status", status)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
