3ca833c55a641f75578b75af9c5bc74c208bd5b3
mikeder <mikedernet@gmail.com>
2018-10-22 22:17:00 -0400

    ssl funkin

--- main.go
+++ main.go
@@ -27,8 +27,37 @@ func status(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", root)
-	http.HandleFunc("/sleep", sleep)
-	http.HandleFunc("/status", status)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+    //#http.HandleFunc("/", root)
+    //#http.HandleFunc("/sleep", sleep)
+    //#http.HandleFunc("/status", status)
+    //#log.Fatal(http.ListenAndServe(":8000", nil))
+    request()
+}
+
+func request() {
+    resp, err := http.Get("https://google.com")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Print("Content Length: ")
+    fmt.Print(resp.ContentLength)
+    fmt.Print("\n")
+    fmt.Print("TLS Version: ")
+    fmt.Print(resp.TLS.Version)
+    fmt.Print("\n")
+    fmt.Print("Handshake Complete: ")
+    fmt.Print(resp.TLS.HandshakeComplete)
+    fmt.Print("\n")
+    fmt.Print("Did Resume: ")
+    fmt.Print(resp.TLS.DidResume)
+    fmt.Print("\n")
+    fmt.Print("Cipher Suite: ")
+    fmt.Print(resp.TLS.CipherSuite)
+    fmt.Print("\n")
+    fmt.Print("Peer Certificates: ")
+    fmt.Print(resp.TLS.PeerCertificates)
+    fmt.Print("\n")
+    fmt.Print("Verified Chains: ")
+    fmt.Print(resp.TLS.VerifiedChains)
+    fmt.Print("\n")
 }
